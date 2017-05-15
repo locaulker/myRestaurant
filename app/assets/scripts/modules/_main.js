1a3877412c3eb3924394d02415cd29cf3c5ab125
Lawrence <locaulker@gmail.com>
2017-05-14 21:38:10 -0400

    Sticky Navigation added

--- app/assets/scripts/modules/_main.js
+++ app/assets/scripts/modules/_main.js
@@ -1,24 +1,29 @@
-//jQuery(document).ready(function(){
-//
-//  /** -- Waypoints: Scroll to -- **/
-//  $('.animation').each(function() {
-//
-//    var waypoint = new Waypoint({
-//      element: this,
-//      handler: function(direction) {
-//        var animation = $(this.element).attr('data-animation');
-//        $(this.element).css('opacity', '1');
-//        $(this.element).addClass("animated " + animation);
-//      },
-//      offset: '30%'
-//    });
-//
-//  });
-//
-//  /** -- Smooth Scrolling -- **/
-//  $('a').smoothScroll({
-//    speed: 1000,
-//    offset: -50
-//  });
-//
-//});
+$(document).ready(function(){
+
+  $('.js--section-features').waypoint(function(direction) {
+
+    if (direction == 'down') {
+      $('nav').addClass('sticky');
+    } else {
+      $('nav').removeClass('sticky');
+    }
+
+  }, {
+    offset: '60px;'
+  });
+
+
+});
+
+
+
+
+
+
+
+
+// var waypoints = $('#handler-first').waypoint(function(direction) {
+//   notify(this.element.id + ' hit 25% from top of window')
+// }, {
+//   offset: '25%'
+// })
